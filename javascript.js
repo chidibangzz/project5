14a6f804d47021971407d11bceb04f55fea46993
Chidi Ekeke <chidiekeke18@yahoo.com>
2020-11-12 19:24:18 -0600

    select container

--- javascript.js
+++ javascript.js
@@ -4,3 +4,12 @@ console.log(timeOfDay);
 
 $("#currentDay").text(`${timeOfDay.format('dddd,MMMMM Do')}`);
 
+$(document).ready(function(){
+    $(".row").hover(function(){
+      $(this).css("background-color", "orange");
+      }, function(){
+      $(this).css("background-color", "grey");
+    });
+  });
+
+  $(".container);
