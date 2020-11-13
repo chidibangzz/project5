e09768a0a0a8d71cea700cec2436fd6afb04855a
Chidi Ekeke <chidiekeke18@yahoo.com>
2020-11-13 00:06:14 -0600

    function

--- javascript.js
+++ javascript.js
@@ -4,6 +4,10 @@ console.log(timeOfDay);
 
 $("#currentDay").text(`${timeOfDay.format('dddd,MMMMM Do')}`);
 
+var hourslot = moment().hour();
+console.log(hourslot);
+
+
 $(document).ready(function(){
     $(".row").hover(function(){
       $(this).css("background-color", "orange");
@@ -12,4 +16,24 @@ $(document).ready(function(){
     });
   });
 
-  $(".container);
+ 
+
+var hourtimes = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
+    hourtimes.forEach(function(hourslot){
+    console.log(hourslot);
+});
+    
+    
+
+function oclock() {
+    var n = document.getUTChours()+1;
+    var hourslot;
+    console.log(hourslot);
+    if (n = n) {
+        $(".saveBtn").addClass(btn)
+    }
+    
+}
+
+
+
